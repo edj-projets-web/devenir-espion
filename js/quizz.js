4170ec6d6b282d942d692539ae2ee0ad29a260cd
Jules Bonnard <julesbonnard@gmail.com>
2017-03-20 17:30:56 +0100

    toggle sections

--- js/quizz.js
+++ js/quizz.js
@@ -1,61 +1,85 @@
 $(function() {
 
-	var profiles = {
-		linguiste: 0,
-		analyste: 0,
-		action: 0,
-		crypto: 0
-	};
+    var profiles = {
+        linguiste: 0,
+        analyste: 0,
+        action: 0,
+        crypto: 0
+    };
 
-	// Lorsque l'on click sur n'importe quel bouton, on gèle tous les boutons présent dans la même <section>
-	$('button').click(function() {
-		$(this).parents('section').find('button').prop('disabled', true);
-	});
+    // Lorsque l'on click sur n'importe quel bouton, on gèle tous les boutons présent dans la même <section>
+    // $('button:not(.dropdown-toggle)').click(function() {
+    //     $(this).parents('section').find('button').prop('disabled', true);
+    // });
 
-	// Lorsque l'on click sur un bouton avec un attribut data-error, affiche un toast avec le contenu de l'attribut
-	$('button[data-error]').click(function() {
-		toastr.error($(this).data('error'))	;
-	});
+    // Lorsque l'on click sur un bouton avec un attribut data-error, affiche un toast avec le contenu de l'attribut
+    $('button[data-error]').click(function() {
+        toastr.error($(this).data('error'));
+    });
 
-	// Lorsque l'on click sur un bouton avec un attribut data-success, affiche un toast avec le contenu de l'attribut
-	$('button[data-success]').click(function() {
-		toastr.success($(this).data('success'))	;
-	});
+    // Lorsque l'on click sur un bouton avec un attribut data-success, affiche un toast avec le contenu de l'attribut
+    $('button[data-success]').click(function() {
+        toastr.success($(this).data('success'));
+    });
 
 
-	//////CARTE
-	// Lorsqu'on clique sur un pays de la carte qui n'est pas le Kenya, on a perdu
-	$('#carte-quizz path:not(#Kenia)').click(function() {
-		toastr.error('tu n\'as pas gagné');
-	});
-	// Lorsqu'on clique sur le Kenya, on a gagné
-	$('#carte-quizz path#Kenia').click(function() {
-		toastr.success('tu as gagné');
-	});
-	////////
+    //////CARTE
+    // Lorsqu'on clique sur un pays de la carte qui n'est pas le Kenya, on a perdu
+    $('#carte-quizz path:not(#Kenia)').click(function() {
+        toastr.error('tu n\'as pas gagné');
+    });
+    // Lorsqu'on clique sur le Kenya, on a gagné
+    $('#carte-quizz path#Kenia').click(function() {
+        toastr.success('tu as gagné');
+    });
+    ////////
 
-	// Lorsque l'on click sur un bouton sur un attribut data-warning, affiche un toast avec le contenu de l'attribut
-	$('button[data-info]').click(function() {
-		toastr.info($(this).data('info')) ;
-	});
+    // Lorsque l'on click sur un bouton sur un attribut data-warning, affiche un toast avec le contenu de l'attribut
+    $('button[data-info]').click(function() {
+        toastr.info($(this).data('info'));
+    });
 
-	// Lorsque l'on click sur un buton avec un attribut data-points, ajoute un nombre de point à chaque profile
-	$('button[data-points]').click(function() {
-		// Créait un tableau avec le nombre de points attribués à chaque profile
-		var lines = $(this).data('points').split(',');
-		// Lit chaque point
-		$(lines).each(function(index, profile) {
-			var points = profile.split(':');
-			profiles[points[0]] += Number(points[1]);
-		});
-		//
-		console.log(profiles);
-	});
+    // Lorsque l'on click sur un buton avec un attribut data-points, ajoute un nombre de point à chaque profile
+    $('button[data-points]').click(function() {
+        // Créait un tableau avec le nombre de points attribués à chaque profile
+        var lines = $(this).data('points').split(',');
+        // Lit chaque point
+        $(lines).each(function(index, profile) {
+            var points = profile.split(':');
+            profiles[points[0]] += Number(points[1]);
+        });
+        //
+        console.log(profiles);
+    });
 
-	// Bootstrap select
-	$('.selectpicker').selectpicker({
-	  style: 'btn-info',
-	  size: 4
-	});
+    // Bootstrap select
+    $('.selectpicker').selectpicker({
+        style: 'btn-info',
+        size: 4
+    });
+
+    displaySection('choix-langues');
+
+    function displaySection(id) {
+    	$('section').hide();
+    	$('section#'+id).show();
+
+    	$('form').submit(function() {
+	    	var next = $(this).data('next');
+	    	displaySection(next);
+	    });
+
+	    switch(id) {
+	    	case 'test-langues': 
+	    		var languesChoices = $('#choix-langues select').val();
+	    		if(languesChoices.length==0) displaySection('geographie');
+	    		languesChoices.forEach(function(val) {
+	    			$('div#test-'+val).show();
+	    		});
+	    		break;
+	    	default:
+	    		break;
+	    }
+    }
 
 });
