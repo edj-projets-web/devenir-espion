78e52f07d4434838520b4ce5b33ae267ce9c1fc8
pirhoo <hello@pirhoo.com>
2017-04-02 19:33:49 +0200

    Added typewriter

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // @see http://www.mattboldt.com/demos/typed-js/
+  Typed.new('.intro__typed', {
+    strings: ["Bonjour, vous êtes admis à passer le concours car vous avez rempli les critères suivants : vous n’avez pas été condamné par la justice, vous avez la nationalité française et vous n’êtes pas binational. Le concours va commencer. Bonne chance."],
+    typeSpeed: 0
+  });
+});
